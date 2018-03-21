97605d4ff7e3049fd305e80255159fc8268d0c5d
shaojianxuan <674749057@qq.com>
2018-03-21 14:36:06 +0800

    新增HelloWord

---
+++ test/HelloWord.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by shaojianxuan on 2018/3/21.
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("HelloWord");
+    }
+}
